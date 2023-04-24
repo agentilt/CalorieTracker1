c8330699a2059d00e931ff51c88e260702ca8767
agentilt <agustingentilt@gmail.com>
2023-04-23 22:55:58 -0500

    foodItem done

--- app/src/main/java/com/csci3397/myapplication/mainactivity/model/foodItems.java
+++ app/src/main/java/com/csci3397/myapplication/mainactivity/model/foodItems.java
@@ -1,6 +1,27 @@
 package com.csci3397.myapplication.mainactivity.model;
-
+import java.util.Date;
 public class foodItems {
+        private String name;
+        private int calories;
+        private Date date;
+
+        public foodItems(String name, int calories) {
+            this.name = name;
+            this.calories = calories;
+            this.date = new Date(); // Set date to current date
+        }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public Date getDate() {
+        return date;
+    }
 
 
 }
